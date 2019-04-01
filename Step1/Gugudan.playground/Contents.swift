bc49729eaf36d6acfa86a9ee03e587bd298063df
allwhite423 <heuin93@naver.com>
2019-04-01 17:15:19 +0900

    Add Gugudan.playground and implement step 1-2

---
+++ Step1/Gugudan.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// step 1-2
+print("2 X 1 = \(2 * 1)")
+print("2 X 2 = \(2 * 2)")
+print("2 X 3 = \(2 * 3)")
+print("2 X 4 = \(2 * 4)")
+print("2 X 5 = \(2 * 5)")
+print("2 X 6 = \(2 * 6)")
+print("2 X 7 = \(2 * 7)")
+print("2 X 8 = \(2 * 8)")
+print("2 X 9 = \(2 * 9)")
+
+print("3 X 1 = \(3 * 1)")
+print("3 X 3 = \(3 * 2)")
+print("3 X 3 = \(3 * 3)")
+print("3 X 4 = \(3 * 4)")
+print("3 X 5 = \(3 * 5)")
+print("3 X 6 = \(3 * 6)")
+print("3 X 7 = \(3 * 7)")
+print("3 X 8 = \(3 * 8)")
+print("3 X 9 = \(3 * 9)")
+
+
+
+
+
